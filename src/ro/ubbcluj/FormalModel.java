e512ae2ace6eb0bbca0fb9618b71dc3121ef22c1
Vladimir <ungureanu.vlad1@gmail.com>
2020-11-28 14:02:16 +0200

    Refactored things for purpose of prettiness of code

--- src/ro/ubbcluj/FormalModel.java
+++ src/ro/ubbcluj/FormalModel.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 import java.util.OptionalInt;
 import java.util.Stack;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import ro.ubbcluj.Grammar.SyntacticalRule;
 
@@ -71,129 +70,120 @@ public class FormalModel {
         while (!Objects.equals(state.getStatus(), "t") &&
                 !Objects.equals(state.getStatus(), "e")) {
 
-            if (Objects.equals(state.getStatus(), "q")) {
-                if (state.getInputStack().isEmpty() &&
-                        state.getPosition() == inputString.size() + 1) {
+            if (isContinueStatus()) {
+                if (isFinalValidState()) {
                     success();
+                } else if (isTopInputStackNonTerminal()) {
+                    expand();
+                } else if (isTopInputStackTerminal() && inputStackMatchesInputString()) { //check if it matches input string
+                    advance();
+                } else {
+                    momentaryInsuccess();
                 }
-                else {
-                    if (grammar.getNonTerminals().contains(state.getInputStack().peek())) {
-                        expand();
-                    } else {
-                        if (grammar.getTerminals().contains(state.getInputStack().peek()) && //check if top of input stack is terminal
-                                state.getInputStack().peek().equals(inputString.get(state.getPosition()))) { //check if it matches input string
-                            advance();
-                        } else {
-                            momentaryInsuccess();
-                        }
-                    }
+            } else if (isBackStatus()) {
+                if (isTopWorkingStackTerminal()) { //check if top of working stack is terminal
+                    back();
                 }
-            }
-            else {
-                if(state.getStatus().equals("r")){
-                    if (grammar.getTerminals().contains(state.getWorkingStack().peek())) { //check if top of working stack is terminal
-                        back();
-                    }
-                }
-                else{
-                    if (isProductionString(state.getInputStack().peek())) {
-                        anotherTry(); //todo double-triple-check for errors
-                    }
-                    else {
-                        if(state.getPosition() == 1 &&
-                                grammar.getNonTerminals().contains(state.getInputStack().peek()) &&
-                                Objects.equals(state.getInputStack().peek(), grammar.getSyntacticalConstruct())){
-                            //todo Andu vrea sa faca incantatii, vrajitorii, magii cu if-uri si construct-uri
+            } else if (isProductionString(state.getInputStack().peek())) {
+                anotherTry(); //todo double-triple-check for errors
+            } else if (state.getPosition() == 1 &&
+                    isTopInputStackNonTerminal() &&
+                    Objects.equals(state.getInputStack().peek(), grammar.getSyntacticalConstruct())) {
+                //todo Andu vrea sa faca incantatii, vrajitorii, magii cu if-uri si construct-uri
 
-                        }
-                    }
-                }
             }
-
         }
+        return false;
+    }
+
+    private boolean isContinueStatus() {
+        return Objects.equals(state.getStatus(), "q");
+    }
+
+    private boolean isBackStatus() {
+        return state.getStatus().equals("r");
+    }
+
+    private boolean isTopWorkingStackTerminal() {
+        return grammar.getTerminals().contains(state.getWorkingStack().peek());
+    }
+
+    private boolean inputStackMatchesInputString() {
+        return state.getInputStack().peek().equals(inputString.get(state.getPosition()));
+    }
+
+    private boolean isTopInputStackNonTerminal() {
+        return grammar.getNonTerminals().contains(state.getInputStack().peek());
+    }
+
+    private boolean isTopInputStackTerminal() {
+        return grammar.getTerminals().contains(state.getInputStack().peek());
+    }
+
+    private boolean isFinalValidState() {
+        return state.getInputStack().isEmpty() &&
+                state.getPosition() == inputString.size() + 1;
     }
 
 
     public void expand() {
-        if (Objects.equals(state.getStatus(), "q") && //if everything is ok
-                grammar.getNonTerminals().contains(state.getInputStack().peek())) {
-            grammar.getSyntacticalRules();
+        OptionalInt indexOfRule = IntStream.range(0, grammar.getSyntacticalRules().size())
+                .filter(index -> Objects.equals(grammar.getSyntacticalRules().get(index).getLeftSide(), state.getInputStack().peek()))
+                .findFirst();
 
-            OptionalInt indexOfRule = IntStream.range(0, grammar.getSyntacticalRules().size())
-                    .filter(index -> Objects.equals(grammar.getSyntacticalRules().get(index).getLeftSide(), state.getInputStack().peek()))
-                    .findFirst();
+        Grammar.SyntacticalRule ruleToBeUsed = grammar.getSyntacticalRules().get(indexOfRule.getAsInt());
 
-            Grammar.SyntacticalRule ruleToBeUsed = grammar.getSyntacticalRules().get(indexOfRule.getAsInt());
+        ruleToBeUsed.getRightSide().forEach(symbol -> {
+            state.getInputStack().push(symbol);
+        });
 
-            ruleToBeUsed.getRightSide().forEach(symbol -> {
-                state.getInputStack().push(symbol);
-            });
-
-            state.getInputStack().push("-" + indexOfRule + "-");
-        }
+        state.getInputStack().push("-" + indexOfRule + "-");
     }
 
     public void advance() {
-        if (Objects.equals(state.getStatus(), "q") && //if everything is ok
-                grammar.getTerminals().contains(state.getInputStack().peek()) && //check if top of input stack is terminal
-                state.getInputStack().peek().equals(inputString.get(state.getPosition()))) { //check input string for validity
-
-            state.setPosition(state.getPosition() + 1);
-            state.getWorkingStack().push(state.getInputStack().pop());
-        }
+        state.setPosition(state.getPosition() + 1);
+        state.getWorkingStack().push(state.getInputStack().pop());
     }
 
 
     public void momentaryInsuccess() {
-        if (Objects.equals(state.getStatus(), "q") && //if everything is ok
-                grammar.getTerminals().contains(state.getInputStack().peek()) && //check if top of input stack is terminal
-                !state.getInputStack().peek().equals(inputString.get(state.position))) { //check input string for invalidity
-
-            state.setStatus("r");
-        }
+        state.setStatus("r");
     }
 
     public void back() {
-        if (Objects.equals(state.getStatus(), "b") //check if we need to "backtrack"
-                && grammar.getTerminals().contains(state.getWorkingStack().peek())) { //check if top of working stack is terminal
+        state.setPosition(state.getPosition() - 1);
+        state.getInputStack().push(state.getWorkingStack().pop());
 
-            state.setPosition(state.getPosition() - 1);
-            state.getInputStack().push(state.getWorkingStack().pop());
-        }
     }
 
     public void anotherTry() {
-        if (isProductionString(state.getInputStack().peek())) {
+        List<SyntacticalRule> syntacticalRules = grammar.getSyntacticalRules();
+        int ruleIndex = parseProductionString(state.getWorkingStack().peek());
 
-            List<SyntacticalRule> syntacticalRules = grammar.getSyntacticalRules();
-            int ruleIndex = parseProductionString(state.getWorkingStack().peek());
+        syntacticalRules.get(ruleIndex).getRightSide().forEach(symbol -> {
+            state.getInputStack().pop();
+        });
+        state.workingStack.pop();
 
-            syntacticalRules.get(ruleIndex).getRightSide().forEach(symbol -> {
-                state.getInputStack().pop();
-            });
-            state.workingStack.pop();
+        IntStream.range(ruleIndex + 1, syntacticalRules.size())
+                .filter(index -> syntacticalRules.get(index).getLeftSide().equals(syntacticalRules.get(ruleIndex).getLeftSide()))
+                .findFirst().ifPresentOrElse(
+                newRuleIndex -> {
+                    state.getWorkingStack().push("-" + newRuleIndex + "-");
 
-            IntStream.range(ruleIndex + 1, syntacticalRules.size())
-                    .filter(index -> syntacticalRules.get(index).getLeftSide().equals(syntacticalRules.get(ruleIndex).getLeftSide()))
-                    .findFirst().ifPresentOrElse(
-                    newRuleIndex -> {
-                        state.getWorkingStack().push("-" + newRuleIndex + "-");
-
-                        syntacticalRules.get(newRuleIndex).getRightSide().forEach(symbol -> {
-                            state.getInputStack().push(symbol);
-                        });
-                    },
-                    () -> {
-                        if (state.getPosition() == 1
-                                && state.getInputStack().peek().equals(grammar.getSyntacticalConstruct())) {
-                            state.setStatus("e");
-                        } else {
-                            state.getInputStack().push(syntacticalRules.get(ruleIndex).getLeftSide());
-                        }
+                    syntacticalRules.get(newRuleIndex).getRightSide().forEach(symbol -> {
+                        state.getInputStack().push(symbol);
+                    });
+                },
+                () -> {
+                    if (state.getPosition() == 1
+                            && state.getInputStack().peek().equals(grammar.getSyntacticalConstruct())) {
+                        state.setStatus("e");
+                    } else {
+                        state.getInputStack().push(syntacticalRules.get(ruleIndex).getLeftSide());
                     }
-            );
-
-        }
+                }
+        );
     }
 
     private boolean isProductionString(String str) {
